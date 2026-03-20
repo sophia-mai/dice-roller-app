f3ddd37411f407d7cee7d89ddb07c6de058e8198
unknown <sophiathanhmai@gmail.com>
2026-03-20 18:45:16 -0400

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/dice-roller-app/", // TODO: Replace REPO_NAME with the name of your repository
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
